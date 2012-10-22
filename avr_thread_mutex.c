11abd781c60dcd83a9593fa7f255409d427c5514
Meitian Huang <_@freeaddr.info>
2012-10-22 18:48:20 +1100

    Remove the redundant implementation of mutex.

--- avr_thread_mutex.c
+++ avr_thread_mutex.c
@@ -6,9 +6,7 @@
 #include "avr_thread.h"
 
 static int8_t   avr_thread_atomic_add(int8_t x, int8_t delta);
-/**
- * avr_thread_basic_mutex
- */
+
 struct avr_thread_basic_mutex *
 avr_thread_basic_mutex_create(void)
 {
@@ -21,6 +19,10 @@ avr_thread_basic_mutex_create(void)
     return mutex;
 }
 
+/*
+ * If a mutex is destoryed and some threads are waiting to acquire it,
+ * there will be a deadlock.
+ */
 void
 avr_thread_basic_mutex_destory(struct avr_thread_basic_mutex *mutex)
 {
@@ -39,15 +41,27 @@ avr_thread_basic_mutex_acquire(struct avr_thread_basic_mutex *mutex)
 {
     uint8_t         sreg;
     while (1) {
+        /*
+         * Yes, I decide to attack this problem by using busy waiting.
+         * This, however, may not be as `busy' as one thinks.
+         * avr_thread_yield() always switches away from the current
+         * thread (unless the active thread is the only runnable one).
+         * Hopefully, other threads will release the mutex before this
+         * thread is switched to.
+         */
         sreg = SREG;
         cli();
+
+        if (mutex == NULL) {
+            break;
+        }
+
         if (mutex->locked == 0) {
             mutex->locked = 1;
             break;
-        } else {
-            SREG = sreg;
-            avr_thread_yield();
         }
+        SREG = sreg;
+        avr_thread_yield();
     }
     SREG = sreg;
     return;
@@ -58,23 +72,31 @@ avr_thread_basic_mutex_release(struct avr_thread_basic_mutex *mutex)
 {
     uint8_t         sreg = SREG;
     cli();
-    if (mutex->locked == 1) {
+
+    if (mutex != NULL && mutex->locked == 1) {
         mutex->locked = 0;
     }
+
     SREG = sreg;
+    // avr_thread_yield(); // optional
     return;
 }
 
-/**
+/*
  * avr_thread_semaphore
  */
 struct avr_thread_semaphore *
 avr_thread_semaphore_create(int value)
 {
     struct avr_thread_semaphore *sem;
+
+    /*
+     * Semaphore is always non-negative.
+     */
     if (value < 0) {
         return NULL;
     }
+
     sem = malloc(sizeof(struct avr_thread_semaphore));
     if (sem == NULL) {
         return NULL;
@@ -87,6 +109,10 @@ avr_thread_semaphore_create(int value)
     return sem;
 }
 
+/*
+ * It would be _your_ fault if you call this function twice on a same
+ * semaphore.
+ */
 void
 avr_thread_semaphore_destroy(struct avr_thread_semaphore *sem)
 {
@@ -95,7 +121,7 @@ avr_thread_semaphore_destroy(struct avr_thread_semaphore *sem)
     cli();
     if (sem != NULL) {
         if (sem->mutex != NULL) {
-            free(sem->mutex);
+            avr_thread_basic_mutex_destory(sem->mutex);
         }
         free(sem);
     }
@@ -106,6 +132,13 @@ avr_thread_semaphore_destroy(struct avr_thread_semaphore *sem)
 void
 avr_thread_sem_up(struct avr_thread_semaphore *sem)
 {
+    if (sem == NULL || sem->mutex == NULL) {
+        /*
+         * TODO
+         * Return error messages.
+         */
+        return;
+    }
     avr_thread_basic_mutex_acquire(sem->mutex);
     ++(sem->lock_count);
     avr_thread_basic_mutex_release(sem->mutex);
@@ -114,80 +147,31 @@ avr_thread_sem_up(struct avr_thread_semaphore *sem)
 void
 avr_thread_sem_down(struct avr_thread_semaphore *sem)
 {
-    avr_thread_basic_mutex_acquire(sem->mutex);
-    while (1) {
-        if (sem->lock_count == 0) {
-            avr_thread_yield();
-        } else {
-            --(sem->lock_count);
-            break;
-        }
-    }
-    avr_thread_basic_mutex_release(sem->mutex);
-}
-
-/**
- * avr_thread_mutex
- */
-void
-avr_thread_mutex_acquire(struct avr_thread_mutex *mutex)
-{
-    struct avr_thread_context *t,
-                   *p;
-
-    while (1) {
-        uint8_t         sreg = SREG;
-        cli();
-        if (mutex->owner == avr_thread_active_context) {
-            mutex->lock_count = 1;
-            SREG = sreg;
-            return;
-        } else if (mutex->lock_count != 0) {
-            mutex->lock_count = 1;
-            SREG = sreg;
-            return;
-        } else if (avr_thread_active_context->waiting_for == NULL) {
-            if (mutex->waiting == NULL) {
-                mutex->waiting = avr_thread_active_context;
-            } else {
-                p = NULL;
-                t = mutex->waiting;
-                while (t != NULL) {
-                    p = t;
-                    t = t->next_waiting;
-                }
-                p->next_waiting = avr_thread_active_context;
-                avr_thread_active_context->next_waiting = NULL;
-            }
-            avr_thread_active_context->waiting_for = mutex;
-        }
-        SREG = sreg;
-        avr_thread_yield();
-    }
-}
-
-void
-avr_thread_mutex_release(struct avr_thread_mutex *mutex)
-{
-    uint8_t         sreg = SREG;
-    cli();
-
-    if (mutex->owner != avr_thread_active_context) {
-        SREG = sreg;
+    if (sem == NULL || sem->mutex == NULL) {
+        /*
+         * TODO
+         * Return error messages.
+         */
         return;
     }
 
-    mutex->lock_count = 0;
-
-    if (mutex->waiting != NULL) {
-        mutex->owner = mutex->waiting;
-        if (mutex->waiting->next_waiting != NULL) {
-            mutex->waiting = mutex->waiting->next_waiting;
-        }
+    /*
+     * acquire the mutex -> check the value ->
+     * a. if the value is 0, release the mutex (so that other threads
+     * can acquire the mutex and change the value) -> start over.
+     * b. otherwise, decrement the value (this opearting is safe as we
+     * are holding the mutex -> release the mutex -> return
+     */
+    while (1) {
+        avr_thread_basic_mutex_acquire(sem->mutex);
+        if (sem->lock_count == 0) {
+            avr_thread_basic_mutex_release(sem->mutex);
+            avr_thread_yield();
+        } 
+        --(sem->lock_count);
+        break;
     }
-
-    SREG = sreg;
-    return;
+    avr_thread_basic_mutex_release(sem->mutex);
 }
 
 struct avr_thread_mutex_rw_lock {
@@ -285,3 +269,4 @@ avr_thread_mutex_rw_wunlock(struct avr_thread_mutex_rw_lock *rwlock)
     }
     avr_thread_basic_mutex_release(rwlock->mutex);
 }
+
